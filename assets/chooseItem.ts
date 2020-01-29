90d86dc301fd00a611e0733255059e8247244677
youyou <leigned@outlook.com>
2020-01-29 22:40:02 +0800

    完成编辑器

--- assets/chooseItem.ts
+++ assets/chooseItem.ts
@@ -1,5 +1,6 @@
 import { EventType } from "./enum"
 import SelectUIManager from "./selectUIManager";
+import config from "./config";
 
 const { ccclass, property } = cc._decorator
 interface PlotChoose {//单个选项
@@ -27,22 +28,30 @@ export default class ChooseItem extends cc.Component {
   }
   showChooseEvent() {
     //调用selectUI
-    SelectUIManager.instance.showUI([{ id: 1, name: '打开特殊页面' }, { id: 2, name: '关闭对话' }], (data) => {
-      this.chooseEvent.node.getChildByName('Background').getChildByName('Label').getComponent(cc.Label).string = data.id
-    }, 'name')
+    SelectUIManager.instance.showUI(
+      config.eventHandle.map((item, index) => {
+        return {
+          id: index + 1,
+          name: item
+        }
+      }), (data) => {
+        this.chooseEvent.node.getChildByName('Background').getChildByName('Label').getComponent(cc.Label).string = data.id
+      }, 'name')
   }
   init(data?: PlotChoose) {
     if (data) {
       this.content.string = data.L
       this.input.string = data.E + ''
+      this.chooseEvent.node.getChildByName('Background').getChildByName('Label').getComponent(cc.Label).string = data.T + ''
     } else {
       this.content.string = ''
       this.input.string = ''
+      this.chooseEvent.node.getChildByName('Background').getChildByName('Label').getComponent(cc.Label).string = '选择事件'
     }
   }
   getData() {
     let choose: PlotChoose = { L: null, T: null, E: null }
-    choose.L = ''
+    choose.L = this.content.string
     choose.T = +this.chooseEvent.node.getChildByName('Background').getChildByName('Label').getComponent(cc.Label).string
     choose.E = this.input.string
     return choose
--- assets/editorUIManager.ts
+++ assets/editorUIManager.ts
@@ -2,6 +2,8 @@
 import ChooseItem from "./chooseItem"
 import { EventType, ActionType } from "./enum"
 import SelectUIManager from "./selectUIManager"
+import { saveAs } from "./fileSave";
+import config from "./config";
 
 const { ccclass, property } = cc._decorator
 interface Plot {
@@ -60,11 +62,17 @@ export default class EditorUIManager extends cc.Component {
 
   actionType: ActionType = null
 
+
+  @property(cc.JsonAsset)
+  dataJson: cc.JsonAsset = null
+  plotData: any[] = []
+
   onLoad() {
     EditorUIManager.instance = this
     //this.setBG()
     this.bindEvent()
     this.init()
+    this.plotData = this.dataJson.json
   }
   itemPool: cc.NodePool = new cc.NodePool()
   bindEvent() {
@@ -73,19 +81,33 @@ export default class EditorUIManager extends cc.Component {
       this.itemPool.put(node)
     }
     this.btn_char.node.on('click', () => {
-      SelectUIManager.instance.showUI([{ id: 1, name: '1' }, { id: 2, name: '2' }], (data) => {
+      SelectUIManager.instance.showUI([{ id: 1, name: '主角' }, { id: 2, name: '村民' }], (data) => {
         console.log('玩家选择角色', data)
         this.btn_char.node.getChildByName('Background').getChildByName('Label').getComponent(cc.Label).string = data.id
       }, 'name')
     }, this)
     this.btn_action.node.on('click', () => {
-      SelectUIManager.instance.showUI([{ id: 1, name: '1' }, { id: 2, name: '2' }], (data) => {
-        this.btn_action.node.getChildByName('Background').getChildByName('Label').getComponent(cc.Label).string = data.id
-      }, 'name')
+      SelectUIManager.instance.showUI(
+        config.actionHandle.map((item, index) => {
+          return {
+            id: index + 1,
+            name: item
+          }
+        })
+        , (data) => {
+          this.btn_action.node.getChildByName('Background').getChildByName('Label').getComponent(cc.Label).string = data.id
+        }, 'name')
     }, this)
     this.btn_loadList.node.on('click', () => {
-      SelectUIManager.instance.showUI([{ id: 1, name: '1' }, { id: 2, name: '2' }], (data) => {
-        this.btn_loadList.node.getChildByName('Background').getChildByName('Label').getComponent(cc.Label).string = data.id
+      SelectUIManager.instance.showUI(this.plotData.map(item => {
+        return {
+          id: item.I,
+          name: item.L
+        }
+      }), (data) => {
+        //this.btn_loadList.node.getChildByName('Background').getChildByName('Label').getComponent(cc.Label).string = data.id
+        console.log('加载成功', this.findPlot(data.id))
+        this.init(this.plotData[this.findPlot(data.id)])
       }, 'name')
     }, this)
 
@@ -143,15 +165,45 @@ export default class EditorUIManager extends cc.Component {
       L: this.input_content.string,
       C: []
     }
+    for (let item in plot) {
+      if (!plot[item]) {
+        console.log('事件有数据为空', item)
+        return
+      }
+    }
     let child = this.chooseListContainer.children
     for (let i = 0; i < child.length; i++) {
-      let plotChoose = child[i].getComponent(ChooseItem)
-      plot.C.push(plotChoose.getData())
+      let plotChoose = child[i].getComponent(ChooseItem).getData()
+      if (!plotChoose['T']) {
+        console.log('选项事件不能为空')
+        return
+      }
+      plot.C.push(plotChoose)
     }
-    console.log('保存成功', plot)
+    let index = this.findPlot(this.input_ID.string)
+    if (index >= 0) {
+      this.plotData[index] = plot
+      console.log('覆盖成功', this.plotData)
+    } else {
+      this.plotData.push(plot)
+      console.log('保存成功', this.plotData)
+    }
     this.init()
   }
   downLoadAllData() {
-
+    let json = JSON.stringify(this.plotData)
+    let blob = new Blob([json], { type: "text/plain;charset=utf-8" })
+    saveAs(blob, "hello world.json");
+  }
+  findPlot(id) {
+    if (!this.plotData) {
+      this.plotData = []
+    }
+    for (let i = 0; i < this.plotData.length; i++) {
+      if (this.plotData[i].I == id) {
+        return i
+      }
+    }
+    return -1
   }
 }
